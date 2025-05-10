ca6da14a4379bc13aa4f3d842c4116d85fc3cd03
Nathaniel Pereira <80730410+NathanielPereira@users.noreply.github.com>
2025-05-10 19:30:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('mensagemBotao').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo à minha página! Espero que você tenha um ótimo dia!';
+}); 
